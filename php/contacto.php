430ae619bd2fff60dd44a9c2bf8fbca4be799173
Nacho Agudo <nachoecha@gmail.com>
2019-04-05 08:18:54 +0200

    formulario de contacto

---
+++ php/contacto.php
@@ -0,0 +1,34 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'nachoecha@gmail.com';
+$titulo = $_POST['asunto'];
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Asunto:\n $asunto\n Mensaje:\n $mensaje";
+
+if ($_POST['submit'])
+{
+	if (mail($para, $titulo, $msjCorreo, $header))
+	{
+		?>
+		<script type="text/javascript" language="javascript">// <![CDATA[
+			alert('Mensaje enviado, muchas gracias.');
+			window.location.href = 'http://www.pyrenemedia.com';
+		// ]]></script>
+		<?php
+
+	}
+	else
+	{
+		?>
+		<script type="text/javascript" language="javascript">// <![CDATA[
+			
+alert('No se ha podido realizar el envio, pruebe de nuevo.');
+
+			window.location.href = 'http://www.pyrenemedia.com';
+		// ]]></script>
+		<?php
+	}
+}
